04c1f4e008fc9b21a77ea30d859990219de6d357
Mikhail <mikhail.khmel@gmail.com>
2020-11-01 02:26:24 +0300

    Настройка через конфиг

--- CellGame.py
+++ CellGame.py
@@ -1,8 +1,15 @@
 import pygame
 import random
 import sys
+import configparser
+
+config = configparser.ConfigParser()
+config.read('settings.conf')
 
 pygame.init()
+font_size = int(config['Default']['font_size'])
+font_pygame = pygame.font.SysFont('Arial', font_size)
+
 
 
 class Cell:
@@ -27,9 +34,10 @@ class Cell:
 #   - текущее правило
 #   - текущее поколение
 
+
 class Game:
-    WINDOW_SIZE = 800
-    CELL_SIZE = 10
+    WINDOW_SIZE = int(config['Default']['window_size'])
+    CELL_SIZE = int(config['Default']['cell_size'])
     width = WINDOW_SIZE // CELL_SIZE
     FPS = 100
 
@@ -39,21 +47,21 @@ class Game:
     play = False
 
     cells: dict
+    generation = 0
+    live_cells = 0
 
     def __init__(self, rule: dict):
         self.birth_rule = rule['B']
         self.survival_rule = rule['S']
         self.surface = pygame.display.set_mode(
-            (self.WINDOW_SIZE, self.WINDOW_SIZE))
+            (self.WINDOW_SIZE+300, self.WINDOW_SIZE))
         self.clock = pygame.time.Clock()
         self.cells = {
             j + self.width * i: Cell(i, j) for i in range(self.width) for j in range(self.width)}
-        # self.make_random_cells()
+        if config['Default'].getboolean('random_cells'):
+            self.make_random_cells()
 
         self.main_cycle()
-
-    def dump(self):
-        return [{key: item.dump() for key, item in self.cells.items()}]
 
     def clean_cells(self):
         self.cells = {
@@ -107,15 +115,39 @@ class Game:
                                                                  self.CELL_SIZE,
                                                                  self.CELL_SIZE,
                                                                  self.CELL_SIZE))
+    
+    def draw_info(self):
+        info_sc = pygame.Surface((300,self.WINDOW_SIZE))
+        info_sc.fill(self.DEAD_COLOR)
+        text_gen = font_pygame.render("GENERATION: " + str(self.generation), 1, self.LIVE_COLOR)
+        text_count_cells = font_pygame.render("ALIVE CELLS: " + str(self.live_cells), 1, self.LIVE_COLOR)
+        text_count_cells1 = font_pygame.render("DEAD CELLS: " + str(abs(len(self.cells)-self.live_cells)), 1, self.LIVE_COLOR)
+
+        info_sc.blit(text_gen, (0,0))
+        info_sc.blit(text_count_cells, (0,font_size))
+        info_sc.blit(text_count_cells1, (0,font_size*2))
+
+        self.surface.blit(info_sc, (self.WINDOW_SIZE,0))
+    
+    def calculate_cells(self):
+        self.live_cells=0
+        for cell in self.cells.values():
+            if cell.status:
+                self.live_cells+=1
+
 
     def main_cycle(self):
         while True:
             self.clock.tick(self.FPS)
             pygame.display.update()
             self.surface.fill((self.DEAD_COLOR))
+            
             self.draw_cells()
+            self.draw_info()
 
             if self.play:
+                self.generation+=1
+                self.calculate_cells()
                 self.update_cells()
 
             for i in pygame.event.get():
@@ -128,9 +160,12 @@ class Game:
                 elif i.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
                     pos = (pos[0] // self.CELL_SIZE, pos[1] // self.CELL_SIZE)
-                    self.cells[pos[1] + pos[0] * self.width].status = True if not self.cells[pos[1] +
+                    try:
+                        self.cells[pos[1] + pos[0] * self.width].status = True if not self.cells[pos[1] +
                                                                                              pos[0] * self.width].status else False
-                    self.update_neighbors()
+                        self.update_neighbors()
+                    except:
+                        pass
 
 
 rule = {'B': '3', 'S': '2,3'}
